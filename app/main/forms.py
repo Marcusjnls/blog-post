5d44beddcd8c5824e6f36c75589f36e2386b37c1
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-22 09:36:08 +0300

    Create BlogForm class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required, Email, Length
+
+class BlogForm(FlaskForm):
+    title = StringField('Title', validators = [Required()])
+    text = TextAreaField('Blog',validators = [Required()])
+    category = SelectField('Category', choices = [('cuisine', 'Cuisine'),('voyage','Voyage'), ('health','Health'),('empower','Empowerment')], validators = [Required()])
+    submit = SubmitField('Post')
+
+
