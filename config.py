cb9772967379bd32ccc5de290810e3ba5fca973e
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-23 10:05:50 +0300

    Create routes for navigating the app

--- config.py
+++ config.py
@@ -1,15 +1,29 @@
+import os
 
 class Config:
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jean-louis:password@localhost/blog'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jean-louis:password@localhost/blog'
     DEBUG = True
 
-config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
-
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
