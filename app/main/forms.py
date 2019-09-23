4ce4e49a04fcc48b37542ce36182f7c11d9b932c
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-23 10:02:35 +0300

    Add classes for comment, update and subscriber forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,4 +8,16 @@ class BlogForm(FlaskForm):
     category = SelectField('Category', choices = [('cuisine', 'Cuisine'),('voyage','Voyage'), ('health','Health'),('empower','Empowerment')], validators = [Required()])
     submit = SubmitField('Post')
 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Update Bio', validators = [Required()])
+    submit = SubmitField('Submit')
 
+class CommentForm(FlaskForm):
+    name = StringField('Your name', validators = [Required(), Length(min = 3, max = 20)])
+    text = TextAreaField('Leave a Comment',validators = [Required()])
+    submit = SubmitField('Add Comment')
+
+class SubscriberForm(FlaskForm):
+    name  = StringField('Your name', validators = [Required()])
+    email = StringField('Your email address', validators = [Required(), Email()])
+    submit = SubmitField('Subscribe')
