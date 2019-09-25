f5e562fdd18afec058049fc010ed645136551962
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-09-25 11:47:15 +0300

    deploying again

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 
 
+app = Flask(__name__)
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
@@ -18,7 +20,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
 
-    app = Flask(__name__)
+    
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
